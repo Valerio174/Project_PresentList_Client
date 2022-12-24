a1a9549a6bfeda7b0ebaea20e8417effa1c1c011
Valbabaw17 <valerio.walbert@condorcet.be>
2022-12-24 10:31:43 +0100

    Ajout d'une redirection (requestdispatcher) pour gérer l'erreur 503

--- src/main/java/be/walbertjossart/Servlets/SignInServlet.java
+++ src/main/java/be/walbertjossart/Servlets/SignInServlet.java
@@ -1,8 +1,9 @@
 package be.walbertjossart.Servlets;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,7 +24,45 @@ import be.walbertjossart.JavaBeans.Users;
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
+		//Get username from request
+		String pseudoParam = request.getParameter("pseudo");
+		//Get password from request
+		String passwordParam = request.getParameter("password");
+
+		String emailParam = request.getParameter("email");
+
+		//Check if we are in the submission case
+		if(request.getParameter("submit") != null) {
+			if(pseudoParam==null || passwordParam==null || pseudoParam.equals("") || passwordParam.equals("") || emailParam==null || emailParam.equals("")){
+				request.setAttribute("error", "Username, password or email empty ! Go back to <a href=\"/Project_PresentList_Client/SignInServlet\">Sign In Page</a>");
+			 	getServletContext().getRequestDispatcher("/WEB-INF/Error.jsp").forward(request, response);
+			 			
+			}
+			else {
+
+				Users users = new Users();
+				users.setPseudo(pseudoParam);
+				users.setPassword(passwordParam);
+				users.setEmail(emailParam);
+
+				request.setAttribute("users", users);
+				try {
+					if(users.insertUsers()) {
+						this.getServletContext().getRequestDispatcher("/WEB-INF/HomePage.jsp").forward(request, response);
+
+					}
+					else {
+						request.setAttribute("error", "An error during the register ! Go back to <a href=\"/Project_PresentList_Client/SignInServlet\">Sign In Page</a>");
+					 	getServletContext().getRequestDispatcher("/WEB-INF/Error.jsp").forward(request, response);
+
+					}
+				} catch (SQLException e) {
+ 					e.printStackTrace();
+				}
+
+			}
+		}	
 	}
 
 }
