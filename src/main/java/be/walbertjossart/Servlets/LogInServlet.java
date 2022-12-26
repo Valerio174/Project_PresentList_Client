4649d847624f0be9103454952782340a0d2c8c02
Valbabaw17 <valerio.walbert@condorcet.be>
2022-12-26 15:52:43 +0100

    Modifier la LogInServlet pour vérifier si l'utilisateur se connecte

--- src/main/java/be/walbertjossart/Servlets/LogInServlet.java
+++ src/main/java/be/walbertjossart/Servlets/LogInServlet.java
@@ -1,10 +1,14 @@
 package be.walbertjossart.Servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
- import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import be.walbertjossart.JavaBeans.Users;
 
@@ -37,13 +41,31 @@ import be.walbertjossart.JavaBeans.Users;
 	 			
 			}
 			else {
-				Users users = new Users();
-				users.setPseudo(pseudoParam);
-				users.setPassword(passwordParam);
-				
-				request.setAttribute("users", users);
+				if(Users.GetUser(pseudoParam, passwordParam) != null) {
+					Users users = Users.GetUser(pseudoParam, passwordParam);
 
-				this.getServletContext().getRequestDispatcher("/WEB-INF/MainPage.jsp").forward(request, response);
+					 //Get the current session
+	                HttpSession session = request.getSession();
+	                //If the session is not new, destroy the session and create a new one.
+	                if(!session.isNew()) {
+	                    session.invalidate();
+	                    session = request.getSession();
+	                }
+	                
+	                request.setAttribute("user", users);
+	                session.setAttribute("user", users);  
+	                //response.encodeURL("user_session");
+	                
+					//this.getServletContext().getRequestDispatcher("/MainPageServlet").forward(request, response);
+	                response.sendRedirect("/Project_PresentList_Client/MainPageServlet");
+
+
+				}else {
+					request.setAttribute("error", "Username or password incorrect or not found ! Go back to <a href=\"/Project_PresentList_Client/LogInServlet\">Log In Page</a>");
+		 			getServletContext().getRequestDispatcher("/WEB-INF/Error.jsp").forward(request, response);
+		 			
+				}
+
 			}
 		}	
 	}
